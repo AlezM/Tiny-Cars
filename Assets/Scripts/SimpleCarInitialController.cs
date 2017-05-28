bf59ab6a481c49145e74bd6d70fcf60ddf1fcb41
Alex Malyutin <alex.malyutin.dev@gmail.com>
2017-05-28 15:05:36 +0300

    RedSmallCar with InitialController

---
+++ Assets/Scripts/SimpleCarInitialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarInitialController : MonoBehaviour {
+
+	public float forwardSpeed = 10f;
+	public float backwardSpeed = 3f;
+	public float rotationSpeed = 10f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float hInput = Input.GetAxis ("Horizontal");
+		float vInput = Input.GetAxis ("Vertical");
+
+		transform.Translate ( transform.up * vInput * ((vInput > 0)? forwardSpeed: backwardSpeed) * 0.01f, Space.World);
+		transform.Rotate (Vector3.back * hInput * vInput * rotationSpeed);
+	}
+}
